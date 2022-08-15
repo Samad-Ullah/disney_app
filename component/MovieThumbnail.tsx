ff97cf33c4566cfbd3b037750180b5139b39837d
samad Ullah <usamad152@gmail.com>
2022-08-15 10:06:30 +0500

    commit for verifying the branch

--- component/MovieThumbnail.tsx
+++ component/MovieThumbnail.tsx
@@ -19,7 +19,6 @@ const MovieThumbnail = ({ result }: any) => {
     setLike(false)
   }
 
-
   return (
     <div
       className="relative z-50  min-w-[250px] min-h-[170px] md:min-w-[330px] md:min-h-[210px] rounded-lg overflow-hidden shadow-xl cursor-pointer border-[3px] border-[#f9f9f9] border-opacity-10  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300"
